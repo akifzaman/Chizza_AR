fe7dd3d729e7cb5c4bea74585223ff5a165dbcc5
Md. Akif Zaman <akifzaman4040@gmail.com>
2023-11-24 19:12:00 +0600

    Models added

--- Assets/ItemController.cs
+++ Assets/ItemController.cs
@@ -1,63 +1,43 @@
 using DG.Tweening;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
 
 public class ItemController : MonoBehaviour
 {
-    public GameObject target;
     public float rotationSpeed = 10.0f;
-    public float val;
     public bool isAllowedToRotate;
     public GameObject ingredient;
-    public void Start()
+
+    void Start()
     {
-        val = 360;
-        RepeatIngredient();
         isAllowedToRotate = true;
+        //RepeatIngredient();
         RotateObject();
-        target = GameObject.FindWithTag("Target");
     }
+
     private void Update()
     {
         if (!isAllowedToRotate)
         {
-            transform.rotation = new Quaternion(0f, 360f, 0f, 0f);
+            transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));    
         }
     }
-    private void ChangeOrientation()
-    {
-        if (target == null) return;
-        else if (OrientationManager.Instance.currentOrientation == DeviceOrientation.Portrait)
-        {
-            Vector3 directionToTarget = new Vector3(-90f, 0, 0);
-            Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
-        }
-        else if (OrientationManager.Instance.currentOrientation == DeviceOrientation.FaceUp)
-        {
-            Vector3 directionToTarget = new Vector3(-15f, 0, 0);
-            Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
-        }
-        //Vector3 directionToTargetTemp = target.transform.position - transform.position;    
-    }
+
     private void RotateObject()
     {
-        transform.DORotate(new Vector3(0f, 360f, 0f), 2f, RotateMode.Fast).
-            OnComplete(ToggleRotationAllowance);
+        transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), 2f);
+        transform.DORotate(new Vector3(0f, -90f, 0f), 2.5f)
+            .OnComplete(() => isAllowedToRotate = false);
     }
-    private void ToggleRotationAllowance()
-    {
-        isAllowedToRotate = false;
-    }
+
     private void RepeatIngredient()
     {
         StartCoroutine(InstantiateIngredients());
     }
+
     IEnumerator InstantiateIngredients()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2.5f);
         Instantiate(ingredient, transform.position, Quaternion.identity);
         RepeatIngredient();
     }
